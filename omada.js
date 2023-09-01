4ab4af863e74e62876b319532b5e773c4715884f
Gerald Wodni <gerald.wodni@gmail.com>
2023-09-01 22:07:01 +0200

    omada api finished: getDnsLocalAnswers with some convenience wrappers

--- omada.js
+++ omada.js
@@ -3,14 +3,19 @@ const path = require("path");
 const util = require("util");
 
 class Omada {
-    constructor(host, port) {
-        this.host = host;
-        this.port = port;
+    constructor( opts ) {
+        /* configuration */
+        this.host = opts.host;
+        this.port = opts.port || 443;
+        this.siteName = opts.site;
+        this.username = opts.username;
+        this.password = opts.password;
+
+        /* runtime */
         this.apiPrefix = "/api";
         this.id = null;
         this.token = null;
         this.cookies = {};
-        this.siteName = 'Vienna';
         this.siteId = null;
     }
 
@@ -29,6 +34,11 @@ class Omada {
             if( this.token != null ) {
                 headers['Csrf-Token'] = this.token;
             }
+
+            if( this.siteId != null )
+                httpPath = httpPath.replace( /\{SITEID\}/g, this.siteId );
+            else
+                httpPath = httpPath.replace( /\{SITEID\}/g, 'null' );
 
             /* add cookies */
             if( Object.keys(this.cookies).length > 0 ) {
@@ -56,10 +66,12 @@ class Omada {
                         const [name, value] = cookieStart.split("=");
                         this.cookies[ name ] = value;
                     });
-                    console.log( this.cookies );
                 }
 
                 //console.log( "RES:", httpPath, res.statusCode, res.headers );
+                if( res.statusCode >= 300 && res.statusCode <= 399 && res.headers.location.indexOf( "/login" ) > 0 )
+                    return reject( new Error( "Login redirect" ) );
+
                 res.on("data", chunk => data.push( chunk ) );
                 res.on("end", () => {
                     data = data.join("");
@@ -81,16 +93,42 @@ class Omada {
             req.end();
         });
     }
+
     makeIdRequest( httpMethod, httpPath, body = null ) {
         httpPath = path.join( "/" + this.id, "api/v2", httpPath );
         return this.makeRequest( httpMethod, httpPath, body );
     }
 
-    makeAutoLoginRequest( httpMethod, httpPath, body = null ) {
+    async makeAutoLoginIdRequest( httpMethod, httpPath, body = null ) {
+        try {
+            return await this.makeIdRequest( httpMethod, httpPath, body );
+        } catch( err ) {
+            if( err.message != "Login redirect" )
+                throw err;
+
+            /* reset cookies & token */
+            this.cookies = [];
+            this.token = null;
+
+            console.log( "Logging into omada..." );
+            /* id (always accessible)? */
+            if( this.id == null )
+                this.id = await this.getId();
+
+            /* login! */
+            await this.login( this.username, this.password );
+
+            /* site id? */
+            if( this.siteId == null )
+                await this.getSites();
+
+            return this.makeIdRequest( httpMethod, httpPath, body );
+        }
     }
 
-    get ( httpPath )        {  return this.makeIdRequest( "GET", httpPath         );  }
-    post( httpPath, body )  {  return this.makeIdRequest( "POST", httpPath, body  );  }
+    /* convenience wrappers */
+    get ( httpPath )        {  return this.makeAutoLoginIdRequest( "GET", httpPath          );  }
+    post( httpPath, body )  {  return this.makeAutoLoginIdRequest( "POST", httpPath, body   );  }
 
     /* api implementation */
     async getId() {
@@ -116,44 +154,69 @@ class Omada {
     }
 
     async getNetworks() {
-        return await this.get(`sites/${this.siteId}/setting/lan/networks?currentPage=1&currentPageSize=999`);
+        return await this.get(`sites/{SITEID}/setting/lan/networks?currentPage=1&currentPageSize=999`);
     }
 
     async getDevices() {
-        return await this.get(`sites/${this.siteId}/devices?currentPage=1&currentPageSize=999`);
+        return await this.get(`sites/{SITEID}/devices?currentPage=1&currentPageSize=999`);
     }
 
     async getDhcpReservations() {
-        return await this.get(`sites/${this.siteId}/setting/service/dhcp?currentPage=1&currentPageSize=999`);
+        return await this.get(`sites/{SITEID}/setting/service/dhcp?currentPage=1&currentPageSize=999`);
+    }
+
+    /* specific DNS interface: get all DHCP reservations and their respective network domains */
+    async getDnsLocalAnswers() {
+        const networks = (await this.getNetworks()).data.reduce( (obj, n) => {
+            obj[ n.id ] = {
+                name: n.name,
+                domain: n.domain,
+            }
+            return obj;
+        }, {});
+
+        const dhcpReservations = (await this.getDhcpReservations()).data;
+
+        const localAnswers = {};
+        for( let r of dhcpReservations ) {
+            const names = [];
+            names.push( r.description );
+            names.push( r.mac );
+            if( r.clientName != r.mac )
+                names.push( r.clientName );
+
+            const domain = networks[ r.netId ].domain;
+            const obj = { address: r.ip };
+
+            for( let name of names ) {
+                localAnswers[ name.toLowerCase() ] = obj;
+                localAnswers[ `${name}.${domain}`.toLowerCase() ] = obj;
+            }
+        }
+
+        return localAnswers;
     }
     
-    async run() {
+    async test() {
         try {
-            this.id = await this.getId();
-            console.log( "Id:", this.id );
-
-            const test = await this.get(`/anon/initialization`);
-            console.log( "Test:", test );
-
-            const login = await this.login( "user", "password" );
-            console.log( "login:", login );
-
-            const sites = await this.getSites();
-            console.log( "sites INSP:", util.inspect(sites, {depth: null}) );
-
-            const networks = await this.getNetworks();
-            console.log( "networks:", util.inspect(networks, {depth: null}) );
-
-            const devices = await this.getDevices();
-            console.log( "devices:", util.inspect(devices, {depth: null}) );
-
-            const dhcpReservations = await this.getDhcpReservations();
-            console.log( "devices:", util.inspect(dhcpReservations, {depth: null}) );
+            console.log( "Networks:", await this.getNetworks() );
+            console.log( "Local Answers:", await this.getDnsLocalAnswers() );
         } catch( err ) {
             console.log( "Error:", err );
         }
     }
 }
 
-(new Omada("10.0.0.2", 8443)).run();
+module.exports = Omada;
+
+/* quick test: displays current localAnswers, and some bonus information */
+if( require.main == module ) {
+    (new Omada({
+        host: process.env.OMADA_HOST,
+        port: process.env.OMADA_PORT,
+        site: process.env.OMADA_SITE,
+        username: process.env.OMADA_USERNAME,
+        password: process.env.OMADA_PASSWORD,
+    })).test();
+}
 
